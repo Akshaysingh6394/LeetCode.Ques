06b1776632393a1cdf6311edefc4c741c0bb2f17
Akshay Singh <singhakshay8081@gmail.com>
2025-07-01 14:44:20 +0530

    Time: 1 ms (83.86%) | Memory: 42.4 MB (7.78%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i=0;i<numRows;i++){
+            List<Integer> row = new ArrayList<>(Collections.nCopies(i+1,1));
+            for(int j=1;j<i;j++){
+                int value = result.get(i-1).get(j-1)+result.get(i-1).get(j);
+                row.set(j,value);
+            }
+            result.add(row);
+        }
+        return result;
+    }
+}
