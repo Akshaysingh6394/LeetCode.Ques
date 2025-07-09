5f8d247bcfabaa12c419c038ce9c8157a6f2807a
Akshay Singh <singhakshay8081@gmail.com>
2025-07-09 14:27:12 +0530

    Time: 1 ms (80.44%) | Memory: 45.5 MB (52.96%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ones = 0;
+        int twos = 0;
+        for(int i:nums){
+            ones = (ones ^ i) & ~(twos);
+            twos = (twos ^ i) & ~(ones);
+        }
+        return ones;
+    }
+}
