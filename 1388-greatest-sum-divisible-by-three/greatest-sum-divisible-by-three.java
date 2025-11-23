c88654b1e291e2d2293cc9151dbb32bab58a09e3
Akshay Singh <singhakshay8081@gmail.com>
2025-11-23 18:12:29 +0530

    Time: 12 ms (58.39%) | Memory: 49.1 MB (52.48%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
@@ -1,16 +1,44 @@
+import java.util.*;
+
 class Solution {
     public int maxSumDivThree(int[] nums) {
-        int[] dp = new int[3];
+        int sum = 0;
+        
+        // Arrays to store numbers giving remainder 1 and 2 when divided by 3
+        int[] remain1 = new int[nums.length];
+        int[] remain2 = new int[nums.length];
+        int count1 = 0, count2 = 0;
         
         for (int num : nums) {
-            int[] temp = dp.clone();
-            for (int t : temp) {
-                int newSum = t + num;
-                dp[newSum % 3] = Math.max(dp[newSum % 3], newSum);
+            sum += num;
+            if (num % 3 == 1) {
+                remain1[count1++] = num;
+            } else if (num % 3 == 2) {
+                remain2[count2++] = num;
             }
         }
         
-        return dp[0];
+        if (sum % 3 == 0) {
+            return sum;
+        }
         
+        // Sort only the filled portions of the arrays
+        Arrays.sort(remain1, 0, count1);
+        Arrays.sort(remain2, 0, count2);
+        
+        int result = 0;
+        int remainder = sum % 3; // 1 or 2
+        
+        if (remainder == 1) {
+            int remove1 = count1 >= 1 ? remain1[0] : Integer.MAX_VALUE;
+            int remove2 = count2 >= 2 ? (remain2[0] + remain2[1]) : Integer.MAX_VALUE;
+            result = Math.max(result, sum - Math.min(remove1, remove2));
+        } else { // remainder == 2
+            int remove1 = count2 >= 1 ? remain2[0] : Integer.MAX_VALUE;
+            int remove2 = count1 >= 2 ? (remain1[0] + remain1[1]) : Integer.MAX_VALUE;
+            result = Math.max(result, sum - Math.min(remove1, remove2));
+        }
+        
+        return result;
     }
 }
